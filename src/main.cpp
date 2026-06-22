1232b55b9925d286eeb538773087cdb2cd2ff707
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-22 19:27:58 +0400

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <SDL.h>
+#include "controls.h"
+#include "player.h"
+
+// Твоя глобальная логика
+int main(int argc, char* argv[]) {
+    // Инициализация SDL, создание окна
+    // Игровой цикл (while running)
+    // Обработка событий
+    // Вызовы controls::update(), player::update()
+    // Вызовы player::draw(), controls::draw()
+    return 0;
+}
