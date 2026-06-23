0fb83169393cfdb2a1a09debf2aa75dbc4a4ce4a
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-23 09:20:50 +0400

    Update engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,7 +1,7 @@
 #include "engine.h"
-#include <android/log.h> // ИСПРАВЛЕНО: убрал лишнее подчеркивание
+#include <android/log.h>
 
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "CubicBattle", __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "CubicBattle", __VA_ARGS__)
 
 int Engine::initDisplay() {
     const EGLint attribs[] = { EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL_NONE };
@@ -12,14 +12,33 @@ int Engine::initDisplay() {
     display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
     eglInitialize(display, 0, 0);
     eglChooseConfig(display, attribs, &config, 1, &numConfigs);
+    
+    if (numConfigs == 0) {
+        LOGE("EGL: Конфигурации не найдены");
+        return -1;
+    }
+
     eglGetConfigAttrib(display, config, EGL_NATIVE_VISUAL_ID, &format);
     ANativeWindow_setBuffersGeometry(app->window, 0, 0, format);
     
     surface = eglCreateWindowSurface(display, config, app->window, NULL);
+    if (surface == EGL_NO_SURFACE) {
+        LOGE("EGL: Ошибка создания поверхности");
+        return -1;
+    }
+
     EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
     context = eglCreateContext(display, config, NULL, contextAttribs);
+    if (context == EGL_NO_CONTEXT) {
+        LOGE("EGL: Ошибка создания контекста");
+        return -1;
+    }
     
-    eglMakeCurrent(display, surface, surface, context);
+    if (eglMakeCurrent(display, surface, surface, context) == EGL_FALSE) {
+        LOGE("EGL: Ошибка eglMakeCurrent");
+        return -1;
+    }
+
     eglQuerySurface(display, surface, EGL_WIDTH, &w);
     eglQuerySurface(display, surface, EGL_HEIGHT, &h);
     
